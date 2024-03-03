6f8a3955ebc15558cb16402c478532a8d2e0de4a
ankit bisht <ab67743@gmail.com>
2024-03-03 19:02:33 +0530

    updated api

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -27,7 +27,7 @@ function RestaurantMenu() {
     resInfo?.cards[0]?.card?.card?.info;
 
   const categories =
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
