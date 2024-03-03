fe490a8e1bb4a7f422f73381d0b8e22ba9493fd2
Ankit Bisht <110447589+ankittbisht@users.noreply.github.com>
2024-03-03 18:38:47 +0530

    Update constant.jsx

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -4,7 +4,7 @@ export const CDN_LINK =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
 export const MENU_API =
-  "https://corsproxy.org/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
 
 export const SWIGGY_API =
-  "https://corsproxy.org/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
