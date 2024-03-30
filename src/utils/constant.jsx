e24a8024a5b3a3b341db577276e5307c35c38d61
ankit bisht <ab67743@gmail.com>
2024-03-30 17:21:51 +0530

    api update

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -3,14 +3,23 @@ export const LOGO_URL =
 export const CDN_LINK =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
-export const MENU_API = "https://corsproxy.org/?" +
-encodeURIComponent(
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4529322&lng=73.86523799999999&restaurantId="
-);
-  // "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
+// export const MENU_API =
+//   "https://corsproxy.org/?" +
+//   encodeURIComponent(
+//     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4529322&lng=73.86523799999999&restaurantId="
+//   );
 
-export const SWIGGY_API =  "https://corsproxy.org/?" +
-encodeURIComponent(
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-);
-  // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
+
+// "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
+
+// export const SWIGGY_API =
+//   "https://corsproxy.org/?" +
+//   encodeURIComponent(
+//     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+//   );
+  export const SWIGGY_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
+// "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
