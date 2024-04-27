5ab6777eb813acf58d16629736971d9a75e5b335
ankit bisht <ab67743@gmail.com>
2024-04-27 11:30:52 +0530

    pic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card(props) {
     resData?.info;
 
   return (
-    <div className="min-h-[360px] h-full rounded overflow-hidden shadow-lg bg-white">
+    <div className="min-h-[400px] h-full rounded overflow-hidden shadow-lg bg-white">
       <img
         className="w-full  h-40 object-cover"
         src={CDN_LINK + cloudinaryImageId}
