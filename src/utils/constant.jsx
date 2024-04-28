dbff2ae427b705ac9d0ead6eb2bd5c4831122d74
ankit bisht <ab67743@gmail.com>
2024-04-28 17:10:06 +0530

    api changes

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -11,7 +11,7 @@ export const CDN_LINK =
 //   );
 
   export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
+  "https://backend-khaopio.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
 
 // "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
 
@@ -21,6 +21,6 @@ export const CDN_LINK =
 //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 //   );
   export const SWIGGY_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  "https://backend-khaopio.onrender.com/api/restaurants?lat=28.65420&lng=77.23730&page_type=DESKTOP_WEB_LISTING"
 
 // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
